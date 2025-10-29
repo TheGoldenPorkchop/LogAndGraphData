0bd70a90d02b9332aa23b831ef2ecee071dde31f
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-10-29 09:26:54 -0600

    Added Controls I guess

--- LogAndGraphData/DataForm.Designer.vb
+++ LogAndGraphData/DataForm.Designer.vb
@@ -22,10 +22,118 @@ Partial Class DataForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GraphPictureBox = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
+        Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileTopStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        CType(Me.GraphPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ButtonGroupBox.SuspendLayout()
+        Me.TopMenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GraphPictureBox
+        '
+        Me.GraphPictureBox.Location = New System.Drawing.Point(12, 27)
+        Me.GraphPictureBox.Name = "GraphPictureBox"
+        Me.GraphPictureBox.Size = New System.Drawing.Size(776, 263)
+        Me.GraphPictureBox.TabIndex = 0
+        Me.GraphPictureBox.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(10, 21)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 34)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(91, 21)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 34)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(172, 21)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 34)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xitButton"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ButtonGroupBox
+        '
+        Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
+        Me.ButtonGroupBox.Controls.Add(Me.Button1)
+        Me.ButtonGroupBox.Controls.Add(Me.Button2)
+        Me.ButtonGroupBox.Location = New System.Drawing.Point(534, 353)
+        Me.ButtonGroupBox.Name = "ButtonGroupBox"
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(254, 72)
+        Me.ButtonGroupBox.TabIndex = 4
+        Me.ButtonGroupBox.TabStop = False
+        Me.ButtonGroupBox.Text = "GroupBox1"
+        '
+        'TopMenuStrip
+        '
+        Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileTopStripMenuItem})
+        Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.TopMenuStrip.Name = "TopMenuStrip"
+        Me.TopMenuStrip.Size = New System.Drawing.Size(800, 28)
+        Me.TopMenuStrip.TabIndex = 5
+        Me.TopMenuStrip.Text = "MenuStrip1"
+        '
+        'FileTopStripMenuItem
+        '
+        Me.FileTopStripMenuItem.Name = "FileTopStripMenuItem"
+        Me.FileTopStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileTopStripMenuItem.Text = "&File"
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip.TabIndex = 6
+        Me.StatusStrip.Text = "StatusStrip1"
+        '
+        'DataForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.Controls.Add(Me.ButtonGroupBox)
+        Me.Controls.Add(Me.GraphPictureBox)
+        Me.Controls.Add(Me.TopMenuStrip)
+        Me.MainMenuStrip = Me.TopMenuStrip
+        Me.Name = "DataForm"
         Me.Text = "Form1"
+        CType(Me.GraphPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ButtonGroupBox.ResumeLayout(False)
+        Me.TopMenuStrip.ResumeLayout(False)
+        Me.TopMenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents GraphPictureBox As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ButtonGroupBox As GroupBox
+    Friend WithEvents TopMenuStrip As MenuStrip
+    Friend WithEvents FileTopStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip As StatusStrip
 End Class
