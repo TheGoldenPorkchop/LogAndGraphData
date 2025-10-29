e8bc8f22eac1caef9c5ed850924388440f7316d5
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-10-29 11:00:56 -0600

    Random Number Around works and some graph stuff i guess

--- LogAndGraphData/DataForm.Designer.vb
+++ LogAndGraphData/DataForm.Designer.vb
@@ -24,7 +24,7 @@ Partial Class DataForm
     Private Sub InitializeComponent()
         Me.GraphPictureBox = New System.Windows.Forms.PictureBox()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GraphButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
         Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
@@ -37,47 +37,52 @@ Partial Class DataForm
         '
         'GraphPictureBox
         '
+        Me.GraphPictureBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GraphPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.GraphPictureBox.Location = New System.Drawing.Point(12, 27)
         Me.GraphPictureBox.Name = "GraphPictureBox"
-        Me.GraphPictureBox.Size = New System.Drawing.Size(776, 263)
+        Me.GraphPictureBox.Size = New System.Drawing.Size(776, 305)
         Me.GraphPictureBox.TabIndex = 0
         Me.GraphPictureBox.TabStop = False
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(10, 21)
+        Me.Button1.Location = New System.Drawing.Point(7, 21)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 34)
+        Me.Button1.Size = New System.Drawing.Size(86, 56)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Button2
+        'GraphButton
         '
-        Me.Button2.Location = New System.Drawing.Point(91, 21)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 34)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.GraphButton.Location = New System.Drawing.Point(99, 21)
+        Me.GraphButton.Name = "GraphButton"
+        Me.GraphButton.Size = New System.Drawing.Size(86, 56)
+        Me.GraphButton.TabIndex = 2
+        Me.GraphButton.Text = "Graph"
+        Me.GraphButton.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(172, 21)
+        Me.ExitButton.Location = New System.Drawing.Point(191, 21)
         Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(75, 34)
+        Me.ExitButton.Size = New System.Drawing.Size(86, 56)
         Me.ExitButton.TabIndex = 3
         Me.ExitButton.Text = "E&xitButton"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
         'ButtonGroupBox
         '
+        Me.ButtonGroupBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
+        Me.ButtonGroupBox.Controls.Add(Me.GraphButton)
         Me.ButtonGroupBox.Controls.Add(Me.Button1)
-        Me.ButtonGroupBox.Controls.Add(Me.Button2)
-        Me.ButtonGroupBox.Location = New System.Drawing.Point(534, 353)
+        Me.ButtonGroupBox.Location = New System.Drawing.Point(506, 338)
         Me.ButtonGroupBox.Name = "ButtonGroupBox"
-        Me.ButtonGroupBox.Size = New System.Drawing.Size(254, 72)
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(282, 87)
         Me.ButtonGroupBox.TabIndex = 4
         Me.ButtonGroupBox.TabStop = False
         Me.ButtonGroupBox.Text = "GroupBox1"
@@ -118,7 +123,7 @@ Partial Class DataForm
         Me.Controls.Add(Me.TopMenuStrip)
         Me.MainMenuStrip = Me.TopMenuStrip
         Me.Name = "DataForm"
-        Me.Text = "Form1"
+        Me.Text = "Graph That Data"
         CType(Me.GraphPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ButtonGroupBox.ResumeLayout(False)
         Me.TopMenuStrip.ResumeLayout(False)
@@ -130,7 +135,7 @@ Partial Class DataForm
 
     Friend WithEvents GraphPictureBox As PictureBox
     Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents GraphButton As Button
     Friend WithEvents ExitButton As Button
     Friend WithEvents ButtonGroupBox As GroupBox
     Friend WithEvents TopMenuStrip As MenuStrip
--- LogAndGraphData/DataForm.vb
+++ LogAndGraphData/DataForm.vb
@@ -1,3 +1,61 @@
-﻿Public Class DataForm
+﻿'Angel Nava
+'Spring 2025
+'RCET2265
+'SayMyName
+'github link
+Option Strict On
+Option Explicit On
 
+
+Public Class DataForm
+
+    'Program Logic-----------------------------------------------------------------------------------------------------------------------
+
+    Function GetRandonNumberAround(thisnumber%, Optional within% = 10) As Integer
+        Dim result As Integer
+
+        result = GetRandomNumber(2 * within) + (thisnumber - within)
+        Return result
+    End Function
+
+    Function GetRandomNumber(max%, Optional min% = 0) As Integer
+        Randomize()
+
+        Return CInt(System.Math.Floor((Rnd() * (max + 1))))
+    End Function
+
+    Function GetData() As Integer
+        Return 5
+    End Function
+
+    Sub GraphData()
+        Dim g As Graphics = GraphPictureBox.CreateGraphics
+        Dim pen As New Pen(Color.Wheat)
+        Dim scaleX As Single = CSng(GraphPictureBox.Width / 100)
+        Dim scaleY As Single = CSng((GraphPictureBox.Height / 100) * -1)
+
+
+        g.TranslateTransform(0, GraphPictureBox.Height)
+        g.ScaleTransform(scaleX, scaleY)
+
+        pen.Width = 0.25
+        g.DrawLine(pen, 5, 50, 95, 50)
+
+        g.Dispose()
+        pen.Dispose()
+    End Sub
+
+    'Event Handlers----------------------------------------------------------------------------------------------------------------------
+
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub GraphButton_Click(sender As Object, e As EventArgs) Handles GraphButton.Click
+        'GraphData()
+        For i = 1 To 100
+            Console.WriteLine(GetRandonNumberAround(50, 10))
+        Next
+    End Sub
 End Class
